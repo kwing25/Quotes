766bffb62b9e85ecccb6033283dee266928029fd
Kendra Wing <kendracwing@icloud.com>
2021-07-13 15:45:27 -0600

    Updated Files

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require("express");
-var app = express();
-var quotes = "./lib/quotes.json"
+// var express = require("express");
+// var app = express();
+// var quotes = "./lib/quotes.json"
 
-// var json = require('./quotes.json');
+// // var json = require('./quotes.json');
 
-app.get("/url", (req, res, next) => {
-    res.json("./lib/quotes.json");
-});
+// app.get("/url", (req, res, next) => {
+//     res.json("./lib/quotes.json");
+// });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+// app.listen(3000, () => {
+//     console.log("Server running on port 3000");
+// });
